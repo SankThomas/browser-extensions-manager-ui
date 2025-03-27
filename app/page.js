790b6228abad74d48070c24ec576d6632767bb72
Thomas Sankara <thomassankara74@gmail.com>
2025-03-27 21:40:47 +0300

    app build complete

--- app/page.js
+++ app/page.js
@@ -1,92 +1,91 @@
-import React from "react";
+"use client";
+
+import ExtensionCard from "@/components/extensioncard";
+import React, { useState } from "react";
+import data from "@/lib/data.json";
+import { Button } from "@/components/ui/button";
+import { CardDescription } from "@/components/ui/card";
+import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
+import { ModeToggle } from "@/components/modetoggle";
 
 export default function Home() {
+  const [filter, setFilter] = useState("All");
+  const [extensions, setExtensions] = useState(data);
+
+  const filters = ["All", "Active", "Inactive"];
+
+  const toggleExtension = (name) => {
+    setExtensions((prevExtensions) =>
+      prevExtensions.map((ext) =>
+        ext.name === name ? { ...ext, isActive: !ext.isActive } : ext,
+      ),
+    );
+  };
+
+  const filteredExtensions = extensions.filter((ext) => {
+    if (filter === "Active") return ext.isActive;
+    if (filter === "Inactive") return !ext.isActive;
+
+    return true;
+  });
+
+  const removeExtension = (name) => {
+    setExtensions((prev) => prev.filter((ext) => ext.name !== name));
+  };
+
   return (
-    <div className="mx-auto max-w-2xl space-y-6 px-8 py-20">
-      <h1 className="text-center text-2xl font-bold">
-        NextJs 15 Starter with ShadCN, TailwindCSS, Prettier Plugin for
-        TailwindCSS, and JavaScript.
-      </h1>
+    <div className="mx-auto min-h-screen max-w-7xl p-8">
+      <div className="flex items-center justify-between gap-4 rounded-lg bg-neutral-100 p-4 dark:bg-[#1e1e2c]">
+        <h2>
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Logo"
+            width={150}
+            height={10}
+          />{" "}
+        </h2>
 
-      <h2 className="text-xl font-bold">Why did I choose these?</h2>
-
-      <ol className="list-decimal space-y-4">
-        <li>
-          ShadCN: An amazing component library:{" "}
-          <a
-            href="https://ui.shadcn.com"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-500 underline"
-          >
-            {" "}
-            https://ui.shadcn.com
-          </a>
-        </li>
-        <li>
-          Prettier Plugin for TailwindCSS sorts classes according to an order
-          agreed upon at Tailwind Labs:{" "}
-          <a
-            href="https://github.com/tailwindlabs/prettier-plugin-tailwindcss"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-500 underline"
-          >
-            https://github.com/tailwindlabs/prettier-plugin-tailwindcss
-          </a>
-        </li>
-        <li>JavaScript, because I don&apos;t know TypeScript.</li>
-        <li>
-          The default font is set to Geist Sans in{" "}
-          <code className="rounded bg-neutral-800 px-1 py-0.5 text-sm">
-            globals.css
-          </code>{" "}
-          - the current default font in NextJs.
-        </li>
-      </ol>
-
-      <div>
-        <p>
-          This example uses{" "}
-          <code className="rounded bg-neutral-800 px-1 py-0.5 text-sm">
-            pnpm
-          </code>{" "}
-          as the package manager. If you are using a different package manager,
-          first delete the{" "}
-          <code className="rounded bg-neutral-800 px-1 py-0.5 text-sm">
-            pnpm-lock.yaml
-          </code>{" "}
-          file before installing{" "}
-          <code className="rounded bg-neutral-800 px-1 py-0.5 text-sm">
-            node_modules
-          </code>
-          , otherwise it will cause a conflict in your project.
-        </p>
-
-        <p className="mt-8">
-          <strong>UPDATE:</strong> I added{" "}
-          <code className="rounded bg-neutral-800 px-1 py-0.5 text-sm">
-            "next/babel"
-          </code>{" "}
-          in the{" "}
-          <code className="rounded bg-neutral-800 px-1 py-0.5 text-sm">
-            .eslintrc.json
-          </code>{" "}
-          file because I was getting a weird error in all the files saying that
-          it could not find the module.
-        </p>
+        <ModeToggle />
       </div>
 
-      <small className="mt-8 block text-center">
-        <a
-          href="https://youtube.com/tsbsankara"
-          target="_blank"
-          rel="noreferrer"
-          className="text-blue-500 underline"
-        >
-          Find me here
-        </a>
-      </small>
+      <div className="my-6 flex flex-wrap items-center justify-between gap-4 rounded-lg">
+        <h1 className="text-2xl font-bold">Extensions List</h1>
+
+        <div className="flex space-x-4">
+          {filters.map((status) => (
+            <Button
+              key={status}
+              variant={filter === status ? "secondary" : "ghost"}
+              onClick={() => setFilter(status)}
+            >
+              {status}
+            </Button>
+          ))}
+        </div>
+      </div>
+
+      <motion.div
+        layout
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 0.3 } }}
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+      >
+        <AnimatePresence>
+          {filteredExtensions.length > 0 ? (
+            filteredExtensions.map((ext, index) => (
+              <ExtensionCard
+                key={index}
+                {...ext}
+                toggleExtension={toggleExtension}
+                removeExtension={removeExtension}
+              />
+            ))
+          ) : (
+            <CardDescription>No extensions found</CardDescription>
+          )}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
